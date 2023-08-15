4e47b822caec07bd0495db45842be0b8c6d2d376
Giorgi Chagelishvili <chagx116@gmail.com>
2023-08-15 14:36:52 +0400

    initial

---
+++ assets/js/app.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+    // Owl carousel
+    $(".owl-carousel").owlCarousel({
+        //Basic Speeds
+        slideSpeed : 200,
+        paginationSpeed : 800,
+     
+        //Autoplay
+        autoPlay : false,
+        goToFirst : true,
+        goToFirstSpeed : 1000,
+     
+        // Navigation
+        navigation : false,
+        navigationText : ["prev","next"],
+        pagination : true,
+        paginationNumbers: true,
+     
+        // Responsive
+        responsive: true,
+        items : 1,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsMobile : [479,1]
+    })
+
+    $('.hamburger-menu-icon').on('click', function() {
+        $('.nav-menu').slideToggle(350);
+    });
+
+    let imgId = 0;
+    let activeIndex = 0;
+    const circles = $('.circle');
+
+    // Function to update the active circle
+    function updateActiveCircle() {
+        circles.removeClass('active');
+        circles.eq(activeIndex).addClass('active');
+    }
+
+    $('.arrow-down').on('click', function() {
+        imgId++;
+        activeIndex++;
+
+        if (imgId > 4) {
+            imgId = 0;
+            activeIndex = 0;
+        }
+
+        $.getJSON('../data.json', function(data) {
+            let src=data[imgId]['src'];
+
+            $('.carousel-img').fadeOut('150', function() {
+                // Change the src attribute
+                $('.carousel-img').attr('src', src);
+
+                // Fade in the new image
+                $('.carousel-img').fadeIn('150');
+                updateActiveCircle()
+            });
+        });
+    });
+
+    $('.arrow-up').on('click', function() {
+        imgId--;
+        activeIndex--;
+
+        if (imgId < 0) {
+            imgId = 4;
+            activeIndex = 4;
+        }
+
+        $.getJSON('data.json', function(data) {
+            let src=data[imgId]['src'];
+              // Fade out the current image
+            $('.carousel-img').fadeOut('150', function() {
+                // Change the src attribute
+                $('.carousel-img').attr('src', src);
+
+                // Fade in the new image
+                $('.carousel-img').fadeIn('150');
+                updateActiveCircle()
+            });
+        });
+    });
+});
